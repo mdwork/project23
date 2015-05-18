f3f18f2e49ccfc8c63dcaa082b86059804ebb101
Zhe1ka <zheka.dzecina@gmail.com>
2015-05-18 10:15:02 +0300

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -8,4 +8,9 @@ $(document).ready(function(){
 
         $this.toggleClass('active');
     });
+
+    $('.btn-radio input').on('change', function(){
+        $(this).closest('.list-btn-radio').find('.btn-radio').removeClass('active');
+        $(this).parent().toggleClass('active');
+    });
 });
