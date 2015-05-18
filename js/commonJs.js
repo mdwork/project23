098a3e9ea1545c7f947141f0f9c2b69ea570b20f
md.works <mazdze@gmail.com>
2015-05-18 13:11:41 +0300

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $('#side-menu > li').on('click', function(){
-        $this = $(this);
+    $('.main-item').on('click', function(){
+        $this = $(this).parent();
 
         $this.find('.sub-menu-side').slideToggle(function(){
             $this.toggleClass('active-block');
@@ -12,5 +12,11 @@ $(document).ready(function(){
     $('.btn-radio input').on('change', function(){
         $(this).closest('.list-btn-radio').find('.btn-radio').removeClass('active');
         $(this).parent().toggleClass('active');
+    });
+
+    $('.list-btn-position li').on('click', function(){
+        $(this).siblings().removeClass('active');
+
+        $(this).addClass('active');
     });
 });
